15ecc2baf53efcfb5a66d33497864ee829c4db1e
Manuimfeld <manu_jadir@hotmail.com>
2023-06-13 16:39:22 -0300

    worker

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Registrar el Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registrado correctamente:', registration)
+      })
+      .catch((error) => {
+        console.error('Error al registrar el Service Worker:', error)
+      })
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
